9c658a6c461266c49b6ee59cc14b71636398bbad
Cameron Nord <cnord6@gmail.com>
2021-11-18 00:02:23 -0700

    add warning and imminent classes bases on task date

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+   // check due date
+    auditTask(taskLi);
+    
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -43,6 +45,24 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+  // get data from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <=2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 // enable sortable
@@ -112,6 +132,12 @@ $("#trash").droppable({
   }
 });
 
+  // due-date picker
+  $("#modalDueDate").datepicker({
+    minDate: 1
+  });
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -160,7 +186,6 @@ $(".list-group").on("click", "p", function() {
   // auto focus new element
   textInput.trigger("focus");
 });
-
 // editable field was un-focused
 $(".list-group").on("blur", "textarea", function() {
   // get current value of textarea
@@ -195,19 +220,29 @@ $(".list-group").on("click", "span", function() {
     .text()
     .trim();
 
+
   // create new input element
   var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
   $(this).replaceWith(dateInput);
+  
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, for a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
 
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -228,6 +263,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+  // pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
@@ -241,5 +278,7 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+
 
 
